15ec81332d1cebc79aa7625c9f32eccb981b99d9
minhajul-karim <minhajul.kaarim@gmail.com>
2021-04-24 10:46:22 +0600

    first commit

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+export function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/signin">
+          <p>I will be the sign in page</p>
+        </Route>
+        <Route path="/signup">
+          <p>I will be the sign up page</p>
+        </Route>
+        <Route path="/browse">
+          <p>I will be the browse page</p>
+        </Route>
+        <Route path="/">
+          <p>I am going to be a cloned Netflix application</p>
+        </Route>
+      </Switch>
+    </Router>
+  )
+}
